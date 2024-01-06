cbd9c0c427eddf079e238ee2cfd19ffe73c31407
oliviagil25 <olivv2525@gmail.com>
2024-01-06 17:16:17 +0100

    Poprawiony SA

--- SA.py
+++ SA.py
@@ -1,7 +1,7 @@
 import math
 import random
 import pandas as pd
-
+import NN
 # Obliczanie całkowitej długości trasy na podstawie ramki danych z odległościami między miastami
 def total_distance(distances_df, order):
     total = 0
@@ -10,11 +10,16 @@ def total_distance(distances_df, order):
     total += distances_df.iloc[order[-1], order[0]]  # Dodanie odległości z powrotem do punktu początkowego
     return total
 
+# Funkcja używana, jeśli chcemy zastosować algorytm dla losowej kolejności miast
+def random_order(df):
+    num_cities = len(df)
+    order = list(range(num_cities))
+    return order
+
 # Algorytm symulowanego wyżarzania
-def simulated_annealing(distances_df, initial_temp, final_temp, alpha, max_iter):
+def simulated_annealing(order, distances_df, initial_temp, final_temp, alpha, max_iter):
     num_cities = len(distances_df)
-    current_order = list(range(num_cities))
-    random.shuffle(current_order)  # Losowa permutacja miast
+    current_order = order
     current_distance = total_distance(distances_df, current_order)
 
     temperature = initial_temp
@@ -39,31 +44,37 @@ def simulated_annealing(distances_df, initial_temp, final_temp, alpha, max_iter)
 
     return current_order, current_distance
 
-distances_df = pd.read_csv('C:/Users/olivv/OneDrive/Pulpit/Dane_TSP_48.csv')
+df1 = pd.read_csv('C:/Users/olivv/OneDrive/Pulpit/Dane_TSP_48.csv')
+df2 = pd.read_csv('C:/Users/olivv/OneDrive/Pulpit/Dane_TSP_76.csv')
+df3 = pd.read_csv('C:/Users/olivv/OneDrive/Pulpit/Dane_TSP_127.csv')
 
-# Uruchomienie algorytmu
-temperatures = ([1000, 2000, 5000, 10000])
-alphas = ([0.9, 0.95, 0.975, 0.99])
-num_of_iterations = ([1000, 2000, 5000, 10000])
+
+temperatures = ([50, 100, 500, 2000])
+alphas = ([0.75, 0.85, 0.95, 0.99])
+num_of_iterations = ([100, 1000, 5000, 10000])
 
 def temperature_influence(temperatures):
     for i in temperatures:
-        best_order, best_distance = simulated_annealing(distances_df, i,  1, 0.975, 1000)
+        best_order, best_distance = simulated_annealing(NN.best_path(df1)[0], df1, i,  1, 0.975, 1000)
         print("Temperatura:", i)
         print("Najlepsza kolejność miast:", best_order)
         print("Najlepsza odległość:", best_distance)
 
 def alpha_influence(alphas):
     for i in alphas:
-        best_order, best_distance = simulated_annealing(distances_df, 1000, 1, i, 1000)
+        best_order, best_distance = simulated_annealing(NN.best_path(df1)[0], df1, 1000, 1, i, 1000)
         print("Alfa:", i)
         print("Najlepsza kolejność miast:", best_order)
         print("Najlepsza odległość:", best_distance)
 
 def num_of_iterations_influence(num_of_iterations):
     for i in num_of_iterations:
-        best_order, best_distance = simulated_annealing(distances_df, 1000, 1, 0.975, i)
+        best_order, best_distance = simulated_annealing(NN.best_path(df1)[0], df1, 1000, 1, 0.975, i)
         print("Liczba iteracji:", i)
         print("Najlepsza kolejność miast:", best_order)
         print("Najlepsza odległość:", best_distance)
 
+
+best_order, best_distance = simulated_annealing(NN.best_path(df3)[0], df3, 100, 1, 0.975, 1000)
+print("Najlepsza kolejność miast:", best_order)
+print("Najlepsza odległość:", best_distance)
--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 import pandas as pd
 import NN
 
-#Wczytanie danych
-df = pd.read_csv('C:/Users/olivv/OneDrive/Pulpit/Dane_TSP_48.csv')
-
-#Utworzenie na razie pustego planu podróży
-itinerary = {
-    "city": [],
-    "distance": []
-}
 
 if __name__ == '__main__':
-    NN.NN_method(df, itinerary)
+    print("Hello World!")
 
 
